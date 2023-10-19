541610bf477a5224f97bef8419a965cf86617fe6
ashwinrachha786 <136466935+ashwinrachha786@users.noreply.github.com>
2023-10-19 10:42:30 +0000

    Pending changes exported from your codespace

---
+++ app/problem/[id]/layout.tsx
@@ -0,0 +1,22 @@
+import { currentUser } from "@clerk/nextjs"
+
+import { SiteFooter } from "@/components/layout/site-footer"
+import { SiteHeader } from "@/components/layout/site-header"
+
+interface DashboardLayoutProps {
+  children: React.ReactNode
+}
+
+export default async function ProblemsTableLayout({
+  children,
+}: DashboardLayoutProps) {
+  const user = await currentUser()
+
+  return (
+    <div className="relative flex min-h-screen flex-col">
+      <SiteHeader user={user} />
+      <div className="mx-auto flex-1">{children}</div>
+      <SiteFooter />
+    </div>
+  )
+}
